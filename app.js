1f722269bf4f8e66d17b214865173c73d3e709c4
canozo <canwoahzo@gmail.com>
2020-03-31 23:46:14 +0000

    Ajustes del server

--- app.js
+++ app.js
@@ -5,21 +5,23 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const http = require('http');
 const cors = require('cors');
+const fs = require('fs');
 
 const uploadRouter = require('./api/upload/upload');
 
 const app = express();
 const server = http.createServer(app);
-const port = process.env.PORT || 5000;
+const port = 5000;
+const origin = process.env.CORS_ORIGIN || '*';
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
 
 app.use('/upload', uploadRouter);
 
-app.get('/', (req, res) => res.json({ status: 'success', msg: 'hola' }));
+app.get('/api', (req, res) => res.json({ status: 'success', msg: 'hola' }));
 
-server.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
+server.listen(port, () => console.log(`Servidor HTTP escuchando (${port})`));
