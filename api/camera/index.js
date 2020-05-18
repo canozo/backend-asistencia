ae76a5a76a96e0bffb999e02b34f75250d8fddeb
canozo <canwoahzo@gmail.com>
2020-05-17 18:54:10 -0600

    Captures api

--- api/camera/index.js
+++ api/camera/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const AWS = require('aws-sdk');
+const path = require('path');
+const fs = require('fs');
 const auth = require('../../middleware/auth');
 const pagination = require('../../middleware/pagination');
 const setUserType = require('../../middleware/setUserType');
@@ -19,6 +22,72 @@ router.get('/', auth.getToken, auth.verify(1), async (req, res) => {
     res.json({ status: 'success', msg: 'Camaras obtenidas', data: result });
   } catch {
     res.status(500).json({ status: 'error', msg: 'Error al obtener camaras' });
+  }
+});
+
+/**
+ * Get attendance log history by this camera
+ * @route GET /api/camera/marked
+ * @permissions camera
+ * @changed
+ */
+router.get('/marked', auth.getToken, auth.verify(4), async (req, res) => {
+  try {
+    const result = await db.query(
+      `select
+      al.id_attendance_log as idLog,
+      student.id_user as idStudent,
+      c.code as classCode,
+      c.class as className,
+      concat_ws(' ', student.names, student.surnames) as student,
+      student.account_number as accountNumber,
+      marked_at as markedAt,
+      capture_key as captureKey
+      from attendance_x_student axs
+      inner join user student
+      on axs.id_student = student.id_user
+      inner join attendance_log al
+      on al.id_attendance_log = axs.id_attendance_log
+      inner join section s
+      on s.id_section = al.id_section
+      inner join class c
+      on c.id_class = s.id_class
+      where id_marked_by = ?`,
+      [req.data.user.idUser],
+    );
+    res.json({ status: 'success', msg: 'Estudiantes marcados obtenidos', data: result });
+  } catch {
+    return res.status(500).json({ status: 'error', msg: 'Error al obtener estudiantes marcados' });
+  }
+});
+
+/**
+ * Get capture key as image
+ * @route GET /api/camera/capture/:key
+ * @permissions camera
+ * @changed
+ */
+router.get('/capture/:key', auth.getToken, auth.verify(4), async (req, res) => {
+  const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_ID,
+  });
+  const local = path.join(__dirname, '..', '..', 'tmp', 'captures', req.params.key);
+
+  try {
+    if (!fs.existsSync(local)) {
+      // download from s3
+      const file = fs.createWriteStream(local);
+      const params = {
+        Key: `captures/${req.params.key}`,
+        Bucket: process.env.MAIN_BUCKET,
+      };
+      s3.getObject(params).createReadStream().pipe(file);
+    }
+
+    res.sendFile(local);
+  } catch (err) {
+    res.status(500).json({ status: 'error', msg: 'Error al obtener imagen de estudiante' });
   }
 });
 
